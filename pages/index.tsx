cf90fc7ed4c4d8f49329b9425ac5aaf31ccb7779
kazuhi-ra <kazuhira4126@gmail.com>
2020-12-24 04:47:50 +0900

    getServerSideProps()でPrismaを叩いてみる

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,21 @@
-const Index = () => {
-  return <div>Index</div>
+import type { GetServerSideProps } from 'next'
+import prisma from '../lib/prisma'
+
+type Props = {
+  count: number
+}
+
+const Index = (props: Props) => {
+  return <div>user count: {props.count}</div>
 }
 
 export default Index
+
+export const getServerSideProps: GetServerSideProps<Props> = async ctx => {
+  const count = await prisma.user.count()
+  return {
+    props: {
+      count,
+    },
+  }
+}
